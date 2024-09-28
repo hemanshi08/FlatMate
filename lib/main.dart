713d9f6cf8071871c3e587b882e35a24d788bfdc
Kashish268 <khkoshiya@gmail.com>
2024-09-28 13:31:35 +0530

    announcement file add by hemanshi

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: ResidentsPage(),
+      home: MaintenancePage(),
     );
   }
 }
