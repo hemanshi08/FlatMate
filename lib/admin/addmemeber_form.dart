4ae8f2a8f6446165070a4bcbf9d208898e7c2ce0
Kashish268 <khkoshiya@gmail.com>
2024-10-11 20:09:42 +0530

    add email configuration

--- lib/admin/addmemeber_form.dart
+++ lib/admin/addmemeber_form.dart
@@ -1,4 +1,6 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import '../email_service.dart'; // Import the updated email service
 
 class AddMemberForm extends StatefulWidget {
   final Function(Map<String, String>) onMemberAdded;
@@ -17,161 +19,221 @@ class _AddMemberFormState extends State<AddMemberForm> {
   final TextEditingController _peopleController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _contactNoController = TextEditingController();
-  bool _hasPets = false; // Checkbox for pets
+  bool _isSendingEmail = false; // Loading state
 
   void _submitForm() {
     if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isSendingEmail = true; // Set loading state
+      });
+
       final newMember = {
         'flatNo': _flatNoController.text,
         'ownerName': _ownerNameController.text,
         'people': _peopleController.text,
         'email': _emailController.text,
         'contactNo': _contactNoController.text,
-        'hasPets': _hasPets.toString(), // Convert bool to string for storage
       };
       widget.onMemberAdded(newMember);
-      Navigator.pop(context); // Go back after adding member
+
+      final flatNo = _flatNoController.text;
+      final username = 'wings_$flatNo';
+      final password = _generateRandomPassword();
+
+      _sendEmailToMember(
+        email: _emailController.text,
+        flatNo: flatNo,
+        ownerName: _ownerNameController.text,
+        people: _peopleController.text,
+        contactNo: _contactNoController.text,
+        username: username,
+        password: password,
+      ).then((_) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Email sent successfully')),
+        );
+      }).catchError((error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to send email: $error')),
+        );
+      }).whenComplete(() {
+        setState(() {
+          _isSendingEmail = false; // Reset loading state
+        });
+        Navigator.pop(context);
+      });
+    } else {
+      setState(() {});
     }
+  }
+
+  Future<void> _sendEmailToMember({
+    required String email,
+    required String flatNo,
+    required String ownerName,
+    required String people,
+    required String contactNo,
+    required String username,
+    required String password,
+  }) async {
+    final emailService = EmailService();
+    await emailService.sendEmail(
+      ownerName: ownerName,
+      username: username,
+      password: password,
+      email: email,
+    );
+  }
+
+  String _generateRandomPassword() {
+    const characters =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#\$%^&*()'; // Include symbols for better security
+    Random random = Random();
+    return String.fromCharCodes(Iterable.generate(
+      8, // Length of the generated password
+      (_) => characters.codeUnitAt(random.nextInt(characters.length)),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 180, // Keep your toolbar height as you wanted
+        toolbarHeight: screenHeight * 0.24,
         backgroundColor: const Color(0xFF06001A),
-        automaticallyImplyLeading:
-            false, // Disable default back button behavior
+        automaticallyImplyLeading: false,
         flexibleSpace: Padding(
-          padding: const EdgeInsets.only(
-              left: 16, top: 50), // Adjust padding as needed
+          padding: EdgeInsets.only(
+              left: screenWidth * 0.04, top: screenHeight * 0.05),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Back Arrow Icon
               IconButton(
-                icon: Icon(
-                  Icons.arrow_back,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
+                icon: Icon(Icons.arrow_back, color: Colors.white),
+                onPressed: () => Navigator.pop(context),
               ),
-              // Title
-              SizedBox(height: 10),
+              SizedBox(height: screenHeight * 0.02),
               Center(
                 child: Text(
-                  "Add Member",
+                  "Add Residents",
                   style: TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
-                    fontSize: 33,
+                    fontSize: screenWidth * 0.08,
                     letterSpacing: 1,
                   ),
                 ),
               ),
-              Spacer(), // Optional: Adjust the spacing between the title and other elements if needed
+              Spacer(),
             ],
           ),
         ),
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextFormField(
-                  controller: _flatNoController,
-                  decoration: InputDecoration(labelText: 'Flat No'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter flat number';
-                    }
-                    return null;
-                  },
+        // Make the body scrollable
+        padding: EdgeInsets.all(screenWidth * 0.08),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: _flatNoController,
+                decoration: InputDecoration(
+                  labelText: 'Flat No (e.g.,A_101)',
+                  border: OutlineInputBorder(),
                 ),
-                TextFormField(
-                  controller: _ownerNameController,
-                  decoration: InputDecoration(labelText: 'Owner Name'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter owner name';
-                    }
-                    return null;
-                  },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter flat number';
+                  }
+                  if (!RegExp(r'^(A_)\d+$').hasMatch(value)) {
+                    return 'Flat number must start with "wings no_flat no" followed by digits';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                controller: _ownerNameController,
+                decoration: InputDecoration(
+                  labelText: 'Owner Name',
+                  border: OutlineInputBorder(),
                 ),
-                TextFormField(
-                  controller: _peopleController,
-                  decoration: InputDecoration(labelText: 'No. of People'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter the number of people';
-                    }
-                    return null;
-                  },
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter owner name' : null,
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                controller: _peopleController,
+                decoration: InputDecoration(
+                  labelText: 'No. of People',
+                  border: OutlineInputBorder(),
                 ),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(labelText: 'Email'),
-                  validator: (value) {
-                    if (value!.isEmpty ||
-                        !RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                      return 'Enter a valid email';
-                    }
-                    return null;
-                  },
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter number of people' : null,
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email (must be Gmail)',
+                  border: OutlineInputBorder(),
                 ),
-                TextFormField(
-                  controller: _contactNoController,
-                  decoration: InputDecoration(labelText: 'Contact No'),
-                  keyboardType: TextInputType.phone,
-                  validator: (value) {
-                    if (value!.isEmpty ||
-                        !RegExp(r'^\d{10}$').hasMatch(value)) {
-                      return 'Enter a valid 10-digit contact number';
-                    }
-                    return null;
-                  },
+                validator: (value) {
+                  if (value!.isEmpty ||
+                      !RegExp(r'^[a-zA-Z0-9._%+-]+@gmail\.com$')
+                          .hasMatch(value)) {
+                    return 'Enter a valid Gmail address';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                controller: _contactNoController,
+                decoration: InputDecoration(
+                  labelText: 'Contact No (10 digits)',
+                  border: OutlineInputBorder(),
                 ),
-                Row(
-                  children: [
-                    Checkbox(
-                      value: _hasPets,
-                      onChanged: (value) {
-                        setState(() {
-                          _hasPets = value!;
-                        });
-                      },
-                    ),
-                    Text('Send Email'),
-                  ],
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFD8AFCC),
-                    padding: EdgeInsets.symmetric(
-                      vertical: 16.0,
-                    ),
-                    minimumSize: Size(double.infinity, 50), // Full-width button
-                  ),
-                  child: Text(
-                    'Submit',
-                    style: TextStyle(
-                      color: const Color(0xFF66123A),
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+                keyboardType: TextInputType.phone,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter contact number';
+                  }
+                  if (!RegExp(r'^\d{10}$').hasMatch(value)) {
+                    return 'Contact number must be exactly 10 digits';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: _isSendingEmail
+                    ? CircularProgressIndicator() // Show loading indicator
+                    : ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFD8AFCC),
+                          padding: EdgeInsets.symmetric(
+                              vertical: screenHeight * 0.01),
+                          minimumSize: Size(double.infinity, 50),
+                        ),
+                        onPressed: _submitForm,
+                        child: Text(
+                          'Send Email',
+                          style: TextStyle(
+                            color: const Color(0xFF66123A),
+                            fontSize: screenWidth * 0.045,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                      ),
+              ),
+            ],
           ),
         ),
       ),
